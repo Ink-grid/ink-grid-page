0a0241d8bc8a3514d7aaa139101a25c29cc037d3
Carloss U <carlitos2017unocc@gmail.com>
2020-01-23 12:23:01 -0500

    #0001 Implementación del Carousel y sus Estilos

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,58 +4,45 @@ import React from 'react';
 import './style.css';
 
 const Hearder = () => {
+
+	
+
 	return (
 		<header
 			className='masthead'
-			// style={{
-			// backgroundImage:
-			// "url('https://www.ink-grid.com/wp-content/uploads/2019/09/001-1400x600.png')"
-			// }}>
 		>
 			<br></br>
 			<br></br>
 			<br></br>
-			<div
-				style={{
-					padding: '1em',
-					backgroundImage:
-						"url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVZhd44ggtMy4yp0sATCvGqulYZpakJgg8TPGTJGNNXNUNkWqh&s')",
-					backgroundColor: '#cccccc',
-					backgroundRepeat: 'no-repeat',
-					backgroundSize: '100% 100%',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					height: '600px'
-				}}>
-				<div
-					style={{
-						// background: 'blue',
-						padding: '1em',
-						width: '50%',
-						display: 'flex',
-						height: '100%',
-						justifyContent: 'space-between'
-					}}>
-					<div style={{ background: 'yellow' }}>
-						<div
-							style={{
-								borderStyle: 'groove',
-								borderColor: '#ffffff',
-								background: 'blue',
-								borderRadius: '75px',
-								alignItems: 'center',
-								justifyContent: 'center',
-								display: 'flex',
-								width: '150px',
-								height: '150px'
-							}}>
-							BBS <br /> Building block solutions
-						</div>
+			
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+					<img src="https://images.pexels.com/photos/340152/pexels-photo-340152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="." />
 					</div>
-					<div>BBB</div>
+					<div class="carousel-item">
+					<img src="https://www.muypymes.com/impresion-pyme/wp-content/uploads/2019/06/HP-Instant-Ink-gratis.jpg" class="d-block w-100" alt="..." />
+					</div>
+					<div class="carousel-item">
+					<img src="https://www.muypymes.com/impresion-pyme/wp-content/uploads/2019/08/HP-OfficeJet-Pro-9000-28.jpg" class="d-block w-100" alt="..." />
+					</div>
 				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
+			
+
 		</header>
 	);
 };
