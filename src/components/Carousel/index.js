91c79199eda515406d31c5010a27c4b8df8e09dc
rony cb <mylovepre16@gmail.com>
2020-02-12 17:54:51 -0500

    modificaciones en le home

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,187 +1,194 @@
-import React, { useEffect } from "react";
-import "./style.css";
-import Ubicacion from "./../Ubicacion";
-import logo from "../../../src/logo.png";
-import DoneOutlineIcon from "@material-ui/icons/DoneOutline";
-import nevado from "../../../src/img/nevado.png";
-import cpa from "../../../src/img/cpa.png";
-import expro from "../../../src/img/expro.png";
+/** @format */
+
+import React, { useEffect } from 'react';
+import './style.css';
+import Ubicacion from './../Ubicacion';
+import logo from '../../../src/logo.png';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import nevado from '../../../src/img/nevado.png';
+import cpa from '../../../src/img/cpa.png';
+import expro from '../../../src/img/expro.png';
 const Carousel = props => {
-  useEffect(() => {
-    document.getElementById("Idmenu").classList.remove("cambiar-fondo-menu");
-    document.getElementById("logo-dark").src = logo;
-    document.getElementById("redes-contacto").classList.remove("mostrar");
-  }, []);
-  return (
-    <React.Fragment>
-      {/* Portada Home */}
-      <div className="portada-home">
-        <img
-          src="https://images.pexels.com/photos/355952/pexels-photo-355952.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="Ink Grid"
-        />
-        <div className="row contenedor-texto container">
-          <div className="col-10 col-md-6 pl-3 pl-lg-5 py-4 border-contenido my-0">
-            <div className="container text-left ">
-              <span>Bienvenido a </span>
-              <h1 className="text-left text-light">Ink Grid </h1>
-              <p className="d-none d-sm-block contenido-texto text-justify pt-3 pl-3">
-                Trabajamos con las más exitosas compañías de Perú para conducir
-                un crecimiento de largo plazo, transformar las operaciones y las
-                organizaciones, así como nutrir el talento
-              </p>
-            </div>
-          </div>
-          {/*  */}
-          {/* <div className="logo d-none d-xl-block col-10 col-md-5">
-            <img src={logo} className="img-fluid" />
+	useEffect(() => {
+		document.getElementById('Idmenu').classList.remove('cambiar-fondo-menu');
+		document.getElementById('logo-dark').src = logo;
+		document.getElementById('redes-contacto').classList.remove('mostrar');
+	}, []);
+	return (
+		<React.Fragment>
+			{/* Portada Home */}
+			<div className='portada-home bg-dark'>
+				<div
+					style={{
+						position: 'absolute',
+						bottom: '0%',
+						right: '0%',
+						height: '70%',
+						width: '50%',
+						backgroundImage:
+							"url('https://www.mckinsey.com//pe/~/media/McKinsey/Locations/South%20America/Peru/Overview/Peru_Hero_GettyImages-665404151_1080.ashx')"
+					}}></div>
+				{/* <img
+					width='200'
+					src='https://www.mckinsey.com//pe/~/media/McKinsey/Locations/South%20America/Peru/Overview/Peru_Hero_GettyImages-665404151_1080.ashx'
+					alt='Ink Grid'
+        /> */}
 
-          </div> */}
-          {/*  */}
+				<div style={{ position: 'absolute', bottom: '10%' }}>
+					<div className='row contenedor-texto container'>
+						<div className='col-10 col-md-6 pl-3 pl-lg-5 py-4 border-contenido my-0'>
+							<div className='container text-left '>
+								<span>Bienvenido a </span>
+								<h1 className='text-left text-light'>Ink Grid </h1>
+								<p
+									style={{ fontSize: '20px' }}
+									className='d-none d-sm-block contenido-texto text-justify pt-3 pl-3'>
+									Trabajamos con las más exitosas compañías de Perú para
+									conducir un crecimiento de largo plazo, transformar las
+									operaciones y las organizaciones, así como nutrir el talento.
+								</p>
+							</div>
+						</div>
+						<div className='content col-12 col-sm-10 d-none  mover-texto'>
+							<div className='visible'>
+								<p className='posicionarTE'>Te </p>
+								<ul className='content-ul'>
+									<li className='content-li'>enseñamos</li>
+									<li className='content-li'>capacitamos</li>
+									<li className='content-li'>motivamos</li>
+									<li className='content-li'>valoramos ...</li>
+								</ul>
+							</div>
+						</div>
+						{/*  */}
+					</div>
+				</div>
+			</div>
+			{/* Portada Home */}
 
-          <div className="content col-12 col-sm-10 d-none  mover-texto">
-            <div className="visible">
-              <p className="posicionarTE">Te </p>
-              <ul className="content-ul">
-                <li className="content-li">enseñamos</li>
-                <li className="content-li">capacitamos</li>
-                <li className="content-li">motivamos</li>
-                <li className="content-li">valoramos ...</li>
-              </ul>
-            </div>
-          </div>
-          {/*  */}
-        </div>
-      </div>
-      {/* Portada Home */}
+			{/*  */}
+			<div className='descripcion1 container p-2 py-5 text-center bg-ligth text-dark'>
+				Desde que abrimos nuestra oficina en Lima en 2006, hemos trabajado con
+				grandes compañías y firmas emprendedoras de rápido crecimiento a lo
+				largo de todos los sectores clave – incluyendo banca, bienes de consumo
+				y recursos naturales, trayendo un expertise profundo en estrategia de
+				crecimiento, mejora operativa, transformación organizacional, marketing
+				y ventas, y digital. También ayudamos a numerosas iniciativas pro bono
+				que trabajan con emprendedores sociales, fortaleciendo instituciones
+				educativas en matemáticas, ciencias y artes, y apoyando el
+				empoderamiento de la mujer.
+			</div>
 
-      {/*  */}
-      <div className="descripcion1 container p-2 py-5 text-center bg-ligth text-dark">
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
-        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
-        las industrias desde el año 1500, cuando un impresor (N. del T. persona
-        que se dedica a la imprenta) desconocido usó una galería de textos y los
-        mezcló de tal manera que logró hacer un libro de textos especimen. No
-        sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno
-        en documentos electrónicos, quedando esencialmente igual al original.
-        Fue popularizado en los 60s con la creación de las hojas "Letraset", las
-        cuales contenian pasajes de Lorem Ipsum, y más recientemente con
-        software de autoedición, como por ejemplo Aldus PageMaker, el cual
-        incluye versiones de Lorem Ipsum
-      </div>
+			<div className='datos-genericos container py-3'>
+				<div className='row'>
+					<div className='datosEspecificos col-12 col-md-4'>
+						<h1 className='p-3 text-center'>+10</h1>
+						<p>años</p>
+						<span>de presencia de InkGrid en Perú</span>
+						<div className='linea lineaD d-none d-md-block'></div>
+					</div>
 
-      <div className="datos-genericos container py-3">
-        <div className="row">
-          <div className="datosEspecificos col-12 col-md-4">
-            <h1 className="p-3 text-center">+10</h1>
-            <p>años</p>
-            <span>de presencia de InkGrid en Perú</span>
-            <div className="linea lineaD d-none d-md-block"></div>
-          </div>
+					<div className='datosEspecificos col-12 col-md-4'>
+						<h1 className='py-3 text-center'>+290</h1>
+						<p>proyectos</p>
+						<span>emprendidos por Ink Grid en Perú</span>
+					</div>
+					<div className='datosEspecificos col-12 col-md-4 text-center'>
+						<h1 className='p-3 text-center'>+70</h1>
+						<p>por ciento</p>
+						<span>de las grandes corporaciones de Perú servidas</span>
+						<div className='linea lineaI d-none d-md-block'> </div>
+					</div>
+				</div>
+			</div>
 
-          <div className="datosEspecificos col-12 col-md-4">
-            <h1 className="py-3 text-center">+290</h1>
-            <p>proyectos</p>
-            <span>emprendidos por Ink Grid en Perú</span>
-          </div>
-          <div className="datosEspecificos col-12 col-md-4 text-center">
-            <h1 className="p-3 text-center">+70</h1>
-            <p>por ciento</p>
-            <span>de las grandes corporaciones de Perú servidas</span>
-            <div className="linea lineaI d-none d-md-block"> </div>
-          </div>
-        </div>
-      </div>
+			<div className='servicios p-3 m-5'>
+				Diseñamos y Operamos Soluciones Empresariales para mejorar tus Activos
+				Organizacionales
+			</div>
 
-      <div className="servicios p-3 m-5">
-        Diseñamos y Operamos Soluciones Empresariales para mejorar tus Activos
-        Organizacionales
-      </div>
+			{/*  educacion Plus */}
 
-      {/*  educacion Plus */}
+			<div className='container-fluid'>
+				<div
+					style={{
+						display: 'flex',
+						color: '#0062cc',
+						justifyContent: 'center',
+						textAlign: 'initial'
+					}}
+					className='row'>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className=' section-subheading'>
+							<DoneOutlineIcon /> Gestión del talento humano
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Diseño organizacional.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Gestión de los grupos de interés.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Inversión y finanzas.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Gestión del ecosistema.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Liderazgo y gobernanza.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Habilitación de la fuerza laboral.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Gest. inteligente y adap. de procesos.
+						</p>
+					</div>
+					<div className='col-12 col-md-5 m-4 sobresalir'>
+						<p className='section-subheading'>
+							<DoneOutlineIcon /> Estándares y maduréz de los equipos. de
+							gestión.
+						</p>
+					</div>
+				</div>
+			</div>
 
-      <div className="container-fluid">
-        <div
-          style={{
-            display: "flex",
-            color: "#0062cc",
-            justifyContent: "center",
-            textAlign: "initial"
-          }}
-          className="row"
-        >
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className=" section-subheading">
-              <DoneOutlineIcon /> Gestión del talento humano
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Diseño organizacional.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Gestión de los grupos de interés.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Inversión y finanzas.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Gestión del ecosistema.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Liderazgo y gobernanza.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Habilitación de la fuerza laboral.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Gest. inteligente y adap. de procesos.
-            </p>
-          </div>
-          <div className="col-12 col-md-5 m-4 sobresalir">
-            <p className="section-subheading">
-              <DoneOutlineIcon /> Estándares y maduréz de los equipos. de
-              gestión.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      {/*  */}
-      <div className=" nosotros my-5 mx-2">
-        <div className="container text-left">
-          <h1>Ya confían en nosotros</h1>
-          <hr className="w-100"></hr>   
-          <div className="py-4 ml-2 row my-3">
-            <div className="offset-sm-1  offset-md-1 col-4 col-sm-3 col-md-2">
-              <img className="logos" src={nevado} alt="Ink-Grid" />
-            </div>
-            <div className="offset-sm-1 offset-md-2  col-4 col-sm-3 col-md-2">
-              <img className="logos" src={expro} alt="Ink-Grid" />
-            </div>
-            <div className="offset-sm-1 offset-md-2 col-4 col-sm-3 col-md-2">
-              <img className="logos img-fluid" src={cpa} alt="Ink-Grid" />
-            </div>
-            
-          </div>
-          <hr className="w-100"></hr> 
-        </div>
-      </div>
-      <Ubicacion />
-    </React.Fragment>
-  );
+			{/*  */}
+			<div className=' nosotros my-5 mx-2'>
+				<div className='container text-left'>
+					<h1>Ya confían en nosotros</h1>
+					<hr className='w-100'></hr>
+					<div className='py-4 ml-2 row my-3'>
+						<div className='offset-sm-1  offset-md-1 col-4 col-sm-3 col-md-2'>
+							<img className='logos' src={nevado} alt='Ink-Grid' />
+						</div>
+						<div className='offset-sm-1 offset-md-2  col-4 col-sm-3 col-md-2'>
+							<img className='logos' src={expro} alt='Ink-Grid' />
+						</div>
+						<div className='offset-sm-1 offset-md-2 col-4 col-sm-3 col-md-2'>
+							<img className='logos img-fluid' src={cpa} alt='Ink-Grid' />
+						</div>
+					</div>
+					<hr className='w-100'></hr>
+				</div>
+			</div>
+			<Ubicacion />
+		</React.Fragment>
+	);
 };
 
 export default Carousel;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,87 +1,92 @@
 /** @format */
 
-import React from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
-import Mail from "@material-ui/icons/MailOutline";
-import logoB from "../../../src/logo-dark.png";
-import ChevronRight from "@material-ui/icons/ChevronRight";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+import Mail from '@material-ui/icons/MailOutline';
+import logoB from '../../../src/logo-dark.png';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="row footer-top  text-center">
-        <div className="col-12 col-md-7 footer-title">
-          Contáctate con Ink-Grid
-        </div>
+	return (
+		<div className='footer'>
+			<div className='row footer-top  text-center'>
+				<div className='col-12 col-md-7 footer-title'>
+					Contáctate con Ink-Grid
+				</div>
 
-        <div className="col-12 col-md-5 correo-enlace">
-          <Link to="/contacto" className="footer-contact">
-            <Mail /> Contacto
-          </Link>
-        </div>
-      </div>
+				<div className='col-12 col-md-5 correo-enlace'>
+					<Link to='/contacto' className='footer-contact'>
+						<Mail /> Contacto
+					</Link>
+				</div>
+			</div>
 
-      <div className="row footer-botton text-center my-5">
-        <div className="col-md-6 logo-ink container">
-          <img src={logoB} />
-        </div>
-        <div className="my-5 my-md-0 col-md-6 suscribete">
-          <div className="texto">SUBSCRIBE TO OUR LATEST INSIGHTS</div>
-          <input className="mt-4 email" placeholder="Email Adress" />
-          <div className="footer-contact btn ml-3">
-            <ChevronRight />
-          </div>
-        </div>
-      </div>
+			<div className='row footer-botton text-center my-5'>
+				<div className='col-md-6 logo-ink container'>
+					<img src={logoB} />
+				</div>
+				<div className='my-5 my-md-0 col-md-6 suscribete'>
+					<div className='texto'>SUBSCRIBE TO OUR LATEST INSIGHTS</div>
+					<input className='mt-4 email' placeholder='Email Adress' />
+					<div className='footer-contact btn ml-3'>
+						<ChevronRight />
+					</div>
+				</div>
+			</div>
 
-      <div className="container">
-        <div className="menu-footer row">
-          <div className="col-12 col-lg-10">
-            <ul className="">
-              <li>Contáctenos</li>
-              <li>FAQ</li>
-              <li>Política de privacidad</li>
-              <li>Política de cookies</li>
-              <li>Términos de Uso</li>
-              {/* <li>Información del idioma local</li> */}
-              <li>Declaración de accesibilidad</li>
-            </ul>
-          </div>
+			<div className='container'>
+				<div className='menu-footer row'>
+					<div className='col-12 col-lg-10'>
+						<ul className=''>
+							<li>Contáctenos</li>
+							<li>FAQ</li>
+							<li>Política de privacidad</li>
+							<li>Política de cookies</li>
+							<li>Términos de Uso</li>
+							{/* <li>Información del idioma local</li> */}
+							<li>Declaración de accesibilidad</li>
+						</ul>
+					</div>
 
-          <div className="col-12 col-lg-2 text-left iconos-footer text-center">
-            <a href="https://pe.linkedin.com/company/ink-grid"  target="_blank"><LinkedInIcon /></a>
-            <a href="https://twitter.com"  target="_blank"><TwitterIcon /></a>
-            <a href="https://www.facebook.com/InkGridx/"  target="_blank"><FacebookIcon /></a> 
-          </div>
+					<div className='col-12 col-lg-2 text-left iconos-footer text-center'>
+						<a href='https://pe.linkedin.com/company/ink-grid' target='_blank'>
+							<LinkedInIcon />
+						</a>
+						<a href='https://twitter.com' target='_blank'>
+							<TwitterIcon />
+						</a>
+						<a href='https://www.facebook.com/InkGridx/' target='_blank'>
+							<FacebookIcon />
+						</a>
+					</div>
 
-          <hr className="w-100"></hr>
-        </div>
-        <div className="footer-end container">
-          <p>
-            <span>Ink Grid </span> - Obtenga nuestras últimas ideas en su
-            iPhone, iPad o dispositivo Android.
-          </p>
-          <p>© 2020 Ink Grid</p>
-        </div>
-      </div>
+					<hr className='w-100'></hr>
+				</div>
+				<div className='footer-end container'>
+					<p>
+						<span>Ink Grid </span>
+					</p>
+					<p>© 2020 Ink Grid</p>
+				</div>
+			</div>
 
-      {/*  */}
+			{/*  */}
 
-      <div className="cm-up" id="cm-up">
-        <input type="button" className="cm-text-up" value="Volver al Inicio" />
-        <span className="cm-icon">
-          <i className="fas fa-chevron-up"></i>
-        </span>
-      </div>
+			<div className='cm-up' id='cm-up'>
+				<input type='button' className='cm-text-up' value='Volver al Inicio' />
+				<span className='cm-icon'>
+					<i className='fas fa-chevron-up'></i>
+				</span>
+			</div>
 
-  {/*  */}
-    </div>
-  );
+			{/*  */}
+		</div>
+	);
 };
 
 export default Footer;
