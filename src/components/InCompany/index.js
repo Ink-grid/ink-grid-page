b2a6577e04d6f35478b81e491067c83e3864fe73
Carloss U <carlitos2017unocc@gmail.com>
2020-02-12 17:24:50 -0500

    0011 InCompany -avanze

--- src/components/InCompany/index.js
+++ src/components/InCompany/index.js
@@ -26,15 +26,87 @@ const InCompany = () => {
           FORMACIÓN <span>IN COMPANY</span>
         </h1>
       </div>
-    <div className="contenido-padre">
-      <p className="mb-2 mt-1 contenido-descr text-center container">
-         El único programa de formación
-        organizacional a tu medida, dirigido para las empresas que quieran
-        liderar el cambio y buscan un crecimiento interno sostenible.
-      </p>
+      <hr className="w-100 d-none mostrarLinea"></hr>
+      <div className="mb-5 mt-1 contenido-padre">
+        <p className=" contenido-descr text-center container">
+          El único programa de formación organizacional a tu medida, dirigido
+          para las empresas que quieran liderar el cambio y buscan un
+          crecimiento interno sostenible.
+        </p>
       </div>
 
-      
+      {/*  */}
+      <div className="objetivos">
+        <div className="container">
+        <h2>OBJETIVO</h2>
+        <p>
+          Integrar una oferta de formación Empresarial y entrenamiento integral,
+          con rigor en la actualización de temas, con aplicaciones de casos de
+          usos especializados según demanda en contextos muy ajustados y con un
+          alto valor añadido digital.
+        </p>
+        </div>
+      </div>
+
+      {/*  */}
+     
+      {/*  */}
+      <section class="features-icons bg-light text-center">
+      <h1>Temas estratégicos, tácticos y operacionales</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>TRANSFORMACIÓN DIGITAL</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>DIRECCIÓN ESTRATÉGICA DE VENTAS Y CULTURA CRM</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>CYBERSECURITY Y ETHICAL HACKING</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>DIRECCIÓN DE PROYECTOS Y GESTIÓN DE PORTAFOLIOS</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>CIENCIA DE DATOS Y BUSINESS ANALYTICS </h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>RPA DENTRO DE LA GESTIÓN Y GOBIERNO DE LOS PROCESOS</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>GESTIÓN DEL CONOCIMIENTO, LIDERAZGO Y CUADRO DE MANDO EN LA DIRECCIÓN DEL TALENTO HUMANO</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        
+                        <h3>BLOCKCHAIN Y MARKETING OMNICANALL</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     </div>
   );
 };
