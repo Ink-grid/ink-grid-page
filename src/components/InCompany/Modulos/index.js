e6e26b462e5a8fd7c1fbd7ea1d129fdb9106ed24
rony cb <mylovepre16@gmail.com>
2020-02-19 19:27:18 -0500

    #10288 correccion reglas de useeffect

--- src/components/InCompany/Modulos/index.js
+++ src/components/InCompany/Modulos/index.js
@@ -1,40 +1,33 @@
-import React, { useEffect,useState } from 'react';
+/** @format */
+
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-const modulos = (props) => {
+const Modulos = props => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-   
-
-    return (
-        <React.Fragment>
-            <div className="banner-modulos">
-                <h1>{props.location.state[0].titulo} </h1>
-            </div>
-            <div className="container modulos">
-
-                <div className="row container">
-                    {
-                        props.location.state.filter((e, index) => index !== 0).map(
-                            (datos, index) => (
-
-                                <div key={index} className="col-12  col-md-6  sesiones">
-
-                                    <h1>{datos.NTema}</h1>
-                                    <div>
-                                        <span>{datos.tema}</span>
-                                    </div>
-
-                                </div>
-                            )
-                        )
-                    }
-
-                </div>
-            </div>
-        </React.Fragment>
-    );
-
+	return (
+		<React.Fragment>
+			<div className='banner-modulos'>
+				<h1>{props.location.state[0].titulo} </h1>
+			</div>
+			<div className='container modulos'>
+				<div className='row container'>
+					{props.location.state
+						.filter((e, index) => index !== 0)
+						.map((datos, index) => (
+							<div key={index} className='col-12  col-md-6  sesiones'>
+								<h1>{datos.NTema}</h1>
+								<div>
+									<span>{datos.tema}</span>
+								</div>
+							</div>
+						))}
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
-export default modulos;
-
-
+export default Modulos;
